43c29e0a099f20b0e4e60bbe9c64bb653a82107e
1 <menathlakvindub@gmail.com>
2024-01-03 23:52:14 +0530

    added enter button to home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import './home.css';
 import Header from "../../components/header/header";
 import TypeTest from "../../components/typeTest/typeTest";
 
 function Home() {
+
+    const [showTypeTest, setShowTypeTest] = useState(false);
+
+    const handleStartTyping = () => {
+        setShowTypeTest(true)
+    }
+
+
+
     return (
         <>
-
-        <Header/>
-        <div className="type-body">
-            < TypeTest />
-        </div>
-
+            <Header/>
+            <div className="type-body">
+            
+                {!showTypeTest && (
+                    <button className="to-enter-button" onClick={setShowTypeTest}>
+                        Start typing..
+                    </button>
+                )}
+                {showTypeTest && <TypeTest/>}
+            
+            </div>
         </>
     ) 
 }
