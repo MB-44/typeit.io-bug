2b3ebb96dbe5c8f508b7cec83e0da222f07acbe2
1 <menathlakvindub@gmail.com>
2024-01-04 10:44:57 +0530

    added cd-timer

---
+++ src/components/countDownTimer/timer.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import "./timer.css";
+
+const CountDownTimer = ({initialTime}) => {
+    const [time, setTime] = useState(initialTime)
+
+    useEffect(() => {
+        if (time > 0) {
+            const intervalId = setInterval(() => {
+                setTime((prevTime) => prevTime - 1);
+            }, 1000);
+
+            return () => clearInterval(intervalId);
+        }
+    }, [time]);
+
+
+    return (
+        <div className='count-down-timer'>
+            {time == 0 ? (
+                <span>Time's up</span>
+            ) : (
+                <span>{formatTime(time)}</span>
+            )}
+        </div>
+    );
+};
+
+const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0': ''}${remainingSeconds}`;
+};
+
+export default CountDownTimer;
