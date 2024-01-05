009053ae75e380ea46cdd409a95b641fb8fd09e1
1 <menathlakvindub@gmail.com>
2024-01-05 21:19:08 +0530

    added login

--- src/pages/sign-up/sign_up.jsx
+++ src/pages/sign-up/sign_up.jsx
@@ -14,17 +14,14 @@ function SignUp() {
     return (
         <>
         <Header/>
+        <div className="container">
             <div className="signup-container">
                 <h2>register </h2>
                 <form action="" className="register-form">
-                    <input type="text" placeholder="username"/>
-                    <br />
-                    <input type="text" placeholder="email"/>
-                    <br />
-                    <input type="text" placeholder="verify email"/>
-                    <br />
-                    <input type="password" placeholder="password"/>
-                    <br />
+                    <input type="text" placeholder="username"/><br />
+                    <input type="text" placeholder="email"/><br />
+                    <input type="text" placeholder="verify email"/><br />
+                    <input type="password" placeholder="password"/><br />
                     <input type="text" placeholder="verify password"/>
                     {!showHome && (
                         <button type="submit" 
@@ -36,6 +33,16 @@ function SignUp() {
                     {showHome && <Home/>}
                 </form>
             </div>
+            <div className="login-container">
+                <h2>login</h2>
+                <form action="" className="login-form">
+                    <input type="text" placeholder="email"/><br />
+                    <input type="text" placeholder="password"/><br />
+                    <input type="checkbox" id="remember-me"/><p>Remember me</p>
+                </form>
+            </div>
+        
+        </div>
         </>
     );
 }
