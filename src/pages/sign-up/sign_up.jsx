5f32c8becd406ca5ecf17b2ce13881da47630d00
1 <menathlakvindub@gmail.com>
2024-01-05 20:23:31 +0530

    added some animations to things u knw

--- src/pages/sign-up/sign_up.jsx
+++ src/pages/sign-up/sign_up.jsx
@@ -15,12 +15,17 @@ function SignUp() {
         <>
         <Header/>
             <div className="signup-container">
-                <h2>Register </h2>
+                <h2>register </h2>
                 <form action="" className="register-form">
-                    <input type="text" placeholder="Enter your Email"/>
+                    <input type="text" placeholder="username"/>
+                    <br />
+                    <input type="text" placeholder="email"/>
+                    <br />
+                    <input type="text" placeholder="verify email"/>
                     <br />
                     <input type="password" placeholder="password"/>
                     <br />
+                    <input type="text" placeholder="verify password"/>
                     {!showHome && (
                         <button type="submit" 
                             onClick={handleToHomePage}
