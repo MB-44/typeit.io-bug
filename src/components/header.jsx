15f0f52c0475b2e76a0a14ee10832c70dbaf9357
1 <menathlakvindub@gmail.com>
2024-01-08 10:22:42 +0530

    updated json

---
+++ src/components/header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { 
+    Header,
+    Container,
+    Burger,
+    Group,
+    createStyles,
+    Paper, Text,
+    Transition
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useRouter } from "next/router";
+import { Link } from "next/link";
+import { ToggleButton } from "@mui/material";
+import { createTestScheduler } from "jest";
+
+const useStyles = createStyles((theme) => {
+    root: {
+
+    }
+})
+
+
+function PageHeader() {
+    const [opened, {toggle, close}] = useDisclosure(false)
+    const HEIGHT = 70
+
+    return (
+        <Header>
+            <Container></Container>
+        </Header>
+    );
+}
+
+export default PageHeader;
